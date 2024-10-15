992a2683f315c17c669cc58f4a65f263fbfbf802
Joseph-m-luna <joseph.luna535@gmail.com>
2024-10-15 16:29:06 -0700

    initial set up of dev environment

---
+++ dataset.py
@@ -0,0 +1,10 @@
+import platform
+
+print(platform.node())
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
+
+print("Path to dataset files:", path)
